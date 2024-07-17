f53d5ff9530f3be160ff0014401e1e37288a9637
Jaroslaw Karwowski (KAJ4WZ) <kaj4wz@bosch.com>
2024-07-17 10:34:00 +0200

    added script that captures "extensive" list of camera topics

---
+++ record_zed_rosbag/record_zed_stereo_gray_extensive.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+readonly SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+readonly ROS_DISTRIBUTION="${1:-humble}"
+readonly TARGET_DIR="${2:-$SCRIPT_DIR}"
+
+source /opt/ros/${ROS_DISTRIBUTION}/setup.bash
+
+cd $TARGET_DIR
+ros2 bag record \
+    /zed/zed_node/imu/data \
+    /zed/zed_node/imu/data_raw \
+    /zed/zed_node/imu/mag \
+    /zed/zed_node/left_gray/image_rect_gray \
+    /zed/zed_node/right_gray/image_rect_gray \
+    /zed/zed_node/left_gray/camera_info \
+    /zed/zed_node/right_gray/camera_info \
+    /zed/zed_node/odom \
+    /zed/zed_node/path_odom \
+    /clicked_point \
+    /diagnostics \
+    /tf \
+    /tf_static \
+    /zed/zed_node/left_cam_imu_transform \
+    /zed/zed_node/depth/depth_registered \
+    /zed/zed_node/point_cloud/cloud_registered \
+    /zed/zed_node/disparity/disparity_image \
+    /zed/zed_node/confidence/confidence_map \
+    /zed/plane \
+    /zed/plane_marker \
+    /zed/zed_node/pose \
+    /zed/zed_node/pose/status \
+    /zed/zed_node/pose_with_covariance \
+    /zed/zed_node/atm_press \
+    /zed/zed_node/temperature/imu \
+    /zed/zed_node/temperature/left \
+    /zed/zed_node/temperature/right
