0b3d83414ac438fc022a3fb35a8db737183b9f8f
Jaroslaw Karwowski (KAJ4WZ) <kaj4wz@bosch.com>
2024-07-17 10:41:52 +0200

    F added script that captures "extensive" list of camera topics

--- record_zed_rosbag/record_zed_stereo_gray_extensive.sh
+++ record_zed_rosbag/record_zed_stereo_gray_extensive.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
 readonly SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-readonly ROS_DISTRIBUTION="${1:-humble}"
-readonly TARGET_DIR="${2:-$SCRIPT_DIR}"
-
-source /opt/ros/${ROS_DISTRIBUTION}/setup.bash
+readonly TARGET_DIR="${1:-$SCRIPT_DIR}"
+echo ""
+echo "Remember to source the ZED-related workspace as some topics are ZED-specific!"
+echo ""
 
 cd $TARGET_DIR
 ros2 bag record \
@@ -18,11 +19,14 @@ ros2 bag record \
     /zed/zed_node/right_gray/camera_info \
     /zed/zed_node/odom \
     /zed/zed_node/path_odom \
+    /zed/zed_node/path_map \
     /clicked_point \
     /diagnostics \
     /tf \
     /tf_static \
     /zed/zed_node/left_cam_imu_transform \
+    /zed/zed_node/depth/camera_info \
+    /zed/zed_node/depth/depth_info \
     /zed/zed_node/depth/depth_registered \
     /zed/zed_node/point_cloud/cloud_registered \
     /zed/zed_node/disparity/disparity_image \
